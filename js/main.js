f5ff30eb294339b2f87f3e75b8c1b77e4dbc415a
Jasper Fernandez <jafernandez@nemsu.edu.ph>
2024-04-19 17:41:12 +0800

    Update navigation menu functionality and styles

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var navLinks = document.querySelectorAll("li a");
   var checkbox = document.querySelector('nav input[type="checkbox"]');
   var ul = document.querySelector('nav input[type="checkbox"] ~ ul');
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navLinks.forEach(function (link) {
+        link.classList.remove("active");
+      });
+      this.classList.add("active");
+      checkbox.checked = false;
+      ul.classList.add("fadeOut");
+      setTimeout(() => {
+        ul.classList.remove("fadeOut");
+      }, 500);
+    });
+  });
 
   checkbox.addEventListener("change", function () {
     if (!checkbox.checked) {
